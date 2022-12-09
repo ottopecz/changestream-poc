8beaea5fd114abeab7dcf3acfe994bfbe40cef2d
Otto Pecz <otto.pecz.developer@gmail.com>
2022-12-09 11:04:30 +0100

    Linting middleware

--- libraries/middleware/src/index.d.ts
+++ libraries/middleware/src/index.d.ts
@@ -1,5 +1,5 @@
-import type { Request, Response, NextFunction } from 'express';
+import type { Request, Response, NextFunction } from 'express'
 
 declare module '@converge-exercise/middleware' {
-  export function openApiErrorHandler(err: any, req: Request, res: Response, next: NextFunction): void
+  export function openApiErrorHandler (err: any, req: Request, res: Response, next: NextFunction): void
 }
