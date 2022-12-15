c2b16e85da4e4ed23ff37f0f17d240b3ed7b9ba3
Otto Pecz <otto.pecz.developer@gmail.com>
2022-12-15 13:29:38 +0000

    Linting mongodb driver

--- libraries/mongo-driver/src/index.d.ts
+++ libraries/mongo-driver/src/index.d.ts
@@ -2,67 +2,67 @@ import { Db } from 'mongodb'
 
 declare module '@converge-exercise/mongo-driver' {
   declare class MongoDBDriver {
-    private readonly config;
-    isConnecting: null | Promise<Db>;
-    client: MongoClient | undefined;
-    db: Db | undefined;
-    constructor({ hosts, database, username, password, isSRVConnection, sslCertificate, tlsCertificateFile, options }: {
-      hosts: string;
-      database: string;
-      username?: string;
-      password?: string;
-      isSRVConnection?: boolean;
-      sslCertificate?: string;
-      tlsCertificateFile?: string;
+    private readonly config
+    isConnecting: null | Promise<Db>
+    client: MongoClient | undefined
+    db: Db | undefined
+    constructor ({ hosts, database, username, password, isSRVConnection, sslCertificate, tlsCertificateFile, options }: {
+      hosts: string
+      database: string
+      username?: string
+      password?: string
+      isSRVConnection?: boolean
+      sslCertificate?: string
+      tlsCertificateFile?: string
       options?: {
-        [p: string]: unknown;
-      };
-    });
+        [p: string]: unknown
+      }
+    })
     /**
      * Censors password in the connection string so it can be thrown
      *  and potentially logged
      */
-    static censorConnectionString(connectionString: string): string;
+    static censorConnectionString (connectionString: string): string
     /**
      * Begin the connection process, save the pending promise on this.isConnecting
      */
-    preConnect(): Promise<Db>;
+    preConnect (): Promise<Db>
     /**
      * Connects to the database and save the returned MongoClient and Db objects
      */
-    connect(): Promise<Db>;
+    connect (): Promise<Db>
     /**
      * Close the currently open connection
      */
-    close(): Promise<void>;
+    close (): Promise<void>
     /**
      * Get a connection
      */
-    getDb(): Promise<Db>;
+    getDb (): Promise<Db>
     /**
      * Read multiple documents
      */
-    read({ collection, query, skip, limit }: {
-      collection: string;
+    read ({ collection, query, skip, limit }: {
+      collection: string
       query: {
-        [p: string]: unknown;
-      };
-      skip?: number;
-      limit?: number;
+        [p: string]: unknown
+      }
+      skip?: number
+      limit?: number
     }): Promise<Array<{
-      [p: string]: unknown;
-    }>>;
+      [p: string]: unknown
+    }>>
     /**
      * Create a single document
      */
-    createOne({ collection, doc }: {
-      collection: string;
+    createOne ({ collection, doc }: {
+      collection: string
       doc: {
-        [p: string]: unknown;
-      };
+        [p: string]: unknown
+      }
     }): Promise<{
-      [p: string]: unknown;
-    }>;
+      [p: string]: unknown
+    }>
   }
-  export default MongoDBDriver;
+  export default MongoDBDriver
 }
